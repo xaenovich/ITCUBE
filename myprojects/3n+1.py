55972297b9a21023558dd0fbd2073b89c53033b8
XÆN <122152033+xaenovich@users.noreply.github.com>
2023-03-07 23:33:17 +0800

    Update 3n+1.py

--- myprojects/3n+1.py
+++ myprojects/3n+1.py
@@ -1,15 +1,18 @@
 from turtle import *
-i = 0
 
+i = 0
 n = int(input())
 
-while n != 1:
+while True:
     if n % 2 != 0:
         n = n * 3 + 1
-    if n % 2 == 0:
-        n = n // 2 
+    elif n % 2 == 0:
+        n = n // 2
+    print(n)
+    if n == 1:
+        break
         
     i += 10
     goto(i,n)
-    print(n)
+    
 done()
