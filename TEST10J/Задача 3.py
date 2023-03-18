cb2e8b724d3b39a26bde71b89e97ee0eb1983595
XÆN <122152033+xaenovich@users.noreply.github.com>
2023-03-18 20:30:13 +0800

    Delete TEST10J directory

--- TEST10J/Задача 3.py
+++
@@ -1,9 +0,0 @@
-g = input('Пол(f или m) : ')
-y = int (input('Возраст : '))
-
-if g == 'f':
-    if y >= 10:
-        if y <= 15:
-            print('YES')
-else:
-    print('NO')
--- TEST10J/задача 1.py
+++
@@ -1,6 +0,0 @@
-year = int(input("напишите год :"))
-
-if year % 100 == 0:
-    print('YES')
-else:
-    print('NO')
